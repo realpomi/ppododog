d6cf8ea4ef6c89203d3e72c01c5a4507fdf1be59
Realpomi <realpomi@gmail.com>
2021-02-25 18:03:41 +0900

    port change

--- source/index.js
+++ source/index.js
@@ -2,10 +2,12 @@ var express = require("express");
 var http = require("http");
 var app = express();
 
+const port = process.env.PORT ?? 3000;
+
 app.use("/", (req, res, next) => {
     res.send("hi");
 })
 
-app.listen(3000, () => {
-    console.log("listen..");
+app.listen(port, () => {
+    console.log("listen.. " + port);
 })
